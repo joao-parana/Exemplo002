bc7844d523d43b36c6cd50f891640c0c56a20923
João Antonio Ferreira <joao.parana@gmal.com>
2014-09-30 19:49:50 -0300

    Playgroun com Cocoa  não executou com versão 6.0.1 do XCode. Precisa da 6.1

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -1,10 +1,12 @@
 // Playground - noun: a place where people can play
 
 import UIKit
-import Cocoa
+//import Cocoa
 
 var str = "Hello, playground"
 
-for i in 0 .. 100 {
+var i = 5;
+//for i in 0 .. 100 {
     var j = i * i * i
-}
+//}
+println("O valor de j é \(j)")
